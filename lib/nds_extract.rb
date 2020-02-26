50a6801d026dc31ac1a00c4329124a780ca8b731
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-26 02:49:11 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,22 +1,19 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  result = {}
+  director_counter = 0 
+  while director_counter < nds.length do
+    director = nds[director_counter][:name]
+    movie_counter = 0 
+    result[director] = 0 
+    while movie_counter < nds[director_counter][:movies].length do
+      result[director] += nds[director_counter][:movies][movie_counter][:worldwide_gross]
+      movie_counter += 1 
+    end
+    director_counter += 1 
+  end
+  return result
 end
